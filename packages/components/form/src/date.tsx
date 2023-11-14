878b4198dfb7c56cca7a551a04bbd5989fae9c81
dinert <1293437362@qq.com>
2023-11-14 10:51:42 +0800

    perf: 解决typescript的冲突

--- packages/components/form/src/date.tsx
+++ packages/components/form/src/date.tsx
@@ -48,7 +48,7 @@ export default defineComponent({
 
         const options = computed(() => {
             const options = props.formItem.options || {on: {}};
-            (options as any).type = props.formItem.type
+            (options).type = props.formItem.type
             return options
         })
 
