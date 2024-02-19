490c2c66d7db01b3ff959416fcd4ac3c4431bed2
Dinert <1293437362@qq.com>
2024-02-19 18:33:17 +0800

    fix: 修改打包路径

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,7 @@
 import {defaultTheme, defineUserConfig } from 'vuepress'
 export default defineUserConfig({
-  base: '/dinert-element-plus-docs/',
+  base: './',
+
   lang: 'zh-CN',
   locales: {
     '/': {
