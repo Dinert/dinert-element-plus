fa0e3db53a58989c1988c7fc2db5c6c2aeaa1567
dinert <1293437362@qq.com>
2024-04-15 15:19:18 +0800

    fix: 打包测试

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,7 +10,7 @@ function _resolve(dir: string) {
     return path.resolve(__dirname, dir)
   }
 export default defineConfig({
-    base:  '/dinert-element-plus/',
+    // base:  '/dinert-element-plus/',
     vite: {
         resolve: {
             alias: {
