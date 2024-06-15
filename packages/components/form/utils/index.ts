5b0da9075fb1d938468588a1673a6b60b7c92133
Dinert <1293437362@qq.com>
2024-06-15 21:42:41 +0800

    fix: 去除console

--- packages/components/form/utils/index.ts
+++ packages/components/form/utils/index.ts
@@ -88,8 +88,6 @@ export const valueMouseEnter = (e: MouseEvent, item: any, value: any, _this) => 
         const tooltipEl = (e.target as any).previousElementSibling
         const tooltipWidth = tooltipEl.offsetWidth
 
-        console.log(el.offsetWidth, tooltipWidth, 'el.offsetWidthel.offsetWidth')
-
         if (tooltipWidth >= textWidth) {
             _this.form.formItem[item.key].tempValueDisabled = false
         } else {
