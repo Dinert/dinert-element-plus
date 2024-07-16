4352d197daf5f3bf97041c88429ea83b4da2a999
dinert <1293437362@qq.com>
2024-07-16 11:42:46 +0800

    feat: 增加dataTransformRod方法的导出

--- packages/index.ts
+++ packages/index.ts
@@ -3,13 +3,13 @@ import * as components from './components/index'
 import TablePage2 from './hooks/TablePage/index'
 import UseDialog2 from './hooks/UseDialog/index'
 import UseForm2 from './hooks/UseForm/index'
+export {dataTransformRod} from './utils/tools'
 
 export * from './components/index'
 export const TablePage = TablePage2
 export const UseDialog = UseDialog2
 export const UseForm = UseForm2
 export * from './hooks/TablePage/types/index'
-
 
 const myPlugin: Plugin = {
     install: (app: App) => {
